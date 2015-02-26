e82e6dff0ad43259591712bc826efb0c0f56621f
Cristhian Lombana <criferlo@gmail.com>
2015-02-25 21:10:47 -0500

    ss

--- default/app/controllers/institucion_controller.php
+++ default/app/controllers/institucion_controller.php
@@ -11,9 +11,76 @@
  *
  * @author cristhian
  */
-class InstitucionController extends ActiveRecord {
-    //put your code here
-    function index(){
+View::template("template_sistema");
+Load::models("tipoinstitucion","tipociudad");
+class InstitucionController extends AppController {
+   
+     function index($page=1){
+        
+        $per = new Tipoinstitucion();
+        //$this->arrPersona = array();
+        $consulta=null;
+        
+        if(!empty(Input::post("busqueda"))){
+          
+                if(Input::post("comboBusqueda")=="0"){
+                    $consulta="nombre like '%".Input::post("busqueda")."%'";
+                }
+                
+            
+        }
+                
+        $this->arrInstitucion =  $per->paginar($consulta,$page);
         
     }
+    
+    function nuevo(){
+        if(Input::hasPost("nombre")){
+            
+            
+            $per = new Tipoinstitucion();
+            $per->nombre=Input::post("nombre");
+            $per->tipociudad_id=Input::post("tipociudad_id");
+            
+            
+            if($per->save()){
+                 Flash::valid("Se grabó la institución correctamente");
+                 Router::redirect("institucion/index");
+            }
+           
+        }
+    }
+    
+     function editar($id){
+        
+        $this->per = new Tipoinstitucion();
+        $this->per->find_first($id);
+        $this->id=$id;
+        
+        if(Input::hasPost("nombre")){
+            
+            
+            $per = new Tipoinstitucion();
+            $per->find_first($id);
+            $per->nombre=Input::post("nombre");
+            $per->tipociudad_id=Input::post("tipociudad_id");
+            
+            
+            if($per->update()){
+                 Flash::valid("Se actualizó la institución correctamente");
+                 Router::redirect("institucion/index");
+            }
+           
+        }
+    }
+    
+    function eliminar($id){
+        $per = new Tipoinstitucion();
+        
+        if($per->delete($id)){
+            Flash::valid("Se eliminó correctamente");
+            Router::redirect("institucion/index");
+        }
+    }
+    
 }
